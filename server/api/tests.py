97b9a3ce678cf07dbdbb4934858147655d9d5a68
Zach Calvert <zacharym.calvert@gmail.com>
2023-11-02 20:34:59 -0700

    remove cache elements from tests

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,6 +1,5 @@
 import json
 
-from django.core.cache import cache
 from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
@@ -30,20 +29,13 @@ class DifferenceTestCase(TestCase):
         self.url = reverse("difference")
 
     def test_difference(self):
-        assert cache.get('differences_10') is None
-
         url = self.url + "?number=10"
-
         response = self.client.get(url)
         assert response.status_code == 200
 
         content = json.loads(response.content)
         assert set(content.keys()) == {'datetime', 'difference', 'last_datetime', 'number', 'occurrences'}
         assert content["difference"] == 2640
-
-        cached_value = cache.get('differences_10')
-        assert cached_value['difference'] == 2640
-        assert cached_value['occurrences'] == 1
 
     def test_bad_value(self):
         url = self.url + "?number=x"
@@ -57,8 +49,6 @@ class PythagoreanTripletTestCase(TestCase):
         self.url = reverse("pythagorean_triplet")
 
     def test_is_pythagorean_triplet(self):
-        assert cache.get('pythagorean_3_4_5') is None
-
         url = self.url + "?a=3&b=4&c=5"
         response = self.client.get(url)
         assert response.status_code == 200
@@ -68,14 +58,7 @@ class PythagoreanTripletTestCase(TestCase):
         assert content["is_triplet"] is True
         assert content["product"] == 60
 
-        cached_value = cache.get('pythagorean_3_4_5')
-        assert cached_value['is_triplet'] is True
-        assert cached_value['product'] == 60
-        assert cached_value['occurrences'] == 1
-
     def test_is_not_pythagorean_triplet(self):
-        assert cache.get('pythagorean_1_2_3') is None
-
         url = self.url + "?a=1&b=2&c=3"
         response = self.client.get(url)
         assert response.status_code == 200
@@ -84,11 +67,6 @@ class PythagoreanTripletTestCase(TestCase):
         assert set(content.keys()) == {'datetime', 'is_triplet', 'last_datetime', 'product', 'occurrences'}
         assert content["is_triplet"] is False
         assert content["product"] == 6
-
-        cached_value = cache.get('pythagorean_1_2_3')
-        assert cached_value['is_triplet'] is False
-        assert cached_value['product'] == 6
-        assert cached_value['occurrences'] == 1
 
     def test_bad_value(self):
         url = self.url + "?a=3&b=4&c=five"

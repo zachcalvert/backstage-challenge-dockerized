14df1155fb2506ed661b1ecf75da9bd5dc8f6baa
Zach Calvert <zacharym.calvert@gmail.com>
2023-11-02 19:41:29 -0700

    Refactor endpoints to use redis
    It's worth mentioning that all the tests are still passing with this change. They probably need to be supplemented to show that the cache is being used

--- server/api/views.py
+++ server/api/views.py
@@ -1,33 +1,46 @@
+from django.core.cache import cache
+
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 
 from api import functions
-from api.models import DifferenceRequest, PythagoreanTripletRequest
 
 
 def difference(request):
     """Returns the difference between:
     1. The sum of the squares of the first n natural numbers
     2. The square of the sum of the same first n natural numbers"""
+
     try:
         number = int(request.GET.get("number"))
     except ValueError:
         return HttpResponse(status=400)
 
-    if last_request := DifferenceRequest.objects.filter(number=number).first():
-        difference = last_request.difference
-        last_datetime = last_request.created_at
+    request_time = timezone.now()
+
+    cache_key = f'difference_{number}'
+    if cached_value := cache.get(cache_key):
+        difference = cached_value['difference']
+        last_datetime = cached_value['last_datetime']
+        occurrences = cached_value['occurrences'] + 1
     else:
         sum_of_squares = functions.sum_of_squares(number)
         square_of_sums = functions.square_of_sums(number)
         difference = abs(sum_of_squares - square_of_sums)
         last_datetime = None
+        occurrences = 1
 
-    difference_request = DifferenceRequest.objects.create(number=number, difference=difference)
-    occurrences = DifferenceRequest.objects.filter(number=number).count()
+    cache.set(
+        key=cache_key,
+        value={
+            'difference': difference,
+            'last_datetime': request_time,
+            'occurrences': occurrences
+        }
+    )
 
     return JsonResponse({
-        "datetime": difference_request.created_at,
+        "datetime": request_time,
         "difference": difference,
         "last_datetime": last_datetime,
         "number": number,
@@ -49,32 +62,35 @@ def pythagorean_triplet(request):
     # They should be sorted, but just in case
     a, b, c = sorted([a, b, c])
 
-    if last_request := PythagoreanTripletRequest.objects.filter(
-        a=a,
-        b=b,
-        c=c
-    ).first():
-        is_triplet = last_request.is_triplet
-        product = last_request.product
-        last_datetime = last_request.created_at
+    request_time = timezone.now()
+
+    cache_key = f'pythagorean_{a}_{b}_{c}'
+    if cached_value := cache.get(cache_key):
+        # read current cache state
+        is_triplet = cached_value['is_triplet']
+        last_datetime = cached_value['last_datetime']
+        occurrences = cached_value['occurrences'] + 1
+        product = cached_value['product']
     else:
         is_triplet = functions.is_pythagorean_triplet(a,b,c)
         product = functions.product_of_three(a,b,c)
         last_datetime = None
+        occurrences = 1
 
-    pythagorean_request = PythagoreanTripletRequest.objects.create(
-        a=a,
-        b=b,
-        c=c,
-        is_triplet=is_triplet,
-        product=product
+    cache.set(
+        key=cache_key,
+        value={
+            'is_triplet': is_triplet,
+            'last_datetime': request_time,
+            'occurrences': occurrences,
+            'product': product,
+        }
     )
-    occurrences = PythagoreanTripletRequest.objects.filter(a=a, b=b, c=c).count()
 
     return JsonResponse({
-        "datetime": pythagorean_request.created_at,
+        "datetime": request_time,
         "is_triplet": is_triplet,
         "last_datetime": last_datetime,
         "occurrences": occurrences,
-        "product": product,
+        'product': product
     })

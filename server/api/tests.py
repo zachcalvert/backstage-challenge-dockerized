5d0a18b3ba6fef18df515d2313ba80559ec1e530
Zach Calvert <zacharym.calvert@gmail.com>
2023-11-02 19:59:17 -0700

    remove models

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,11 +1,11 @@
 import json
 
+from django.core.cache import cache
 from django.test import TestCase
 from django.test.client import Client
 from django.urls import reverse
 
-from api.functions import sum_of_squares, square_of_sums, is_pythagorean_triplet
-from api.models import DifferenceRequest, PythagoreanTripletRequest
+from api.functions import sum_of_squares, square_of_sums
 
 
 class SumOfSquaresTestCase(TestCase):
@@ -30,7 +30,7 @@ class DifferenceTestCase(TestCase):
         self.url = reverse("difference")
 
     def test_difference(self):
-        assert DifferenceRequest.objects.count() == 0
+        assert cache.get('differences_10') is None
 
         url = self.url + "?number=10"
 
@@ -40,14 +40,15 @@ class DifferenceTestCase(TestCase):
         content = json.loads(response.content)
         assert set(content.keys()) == {'datetime', 'difference', 'last_datetime', 'number', 'occurrences'}
         assert content["difference"] == 2640
-        assert DifferenceRequest.objects.count() == 1
+
+        cached_value = cache.get('differences_10')
+        assert cached_value['difference'] == 2640
+        assert cached_value['occurrences'] == 1
 
     def test_bad_value(self):
         url = self.url + "?number=x"
-
         response = self.client.get(url)
         assert response.status_code == 400
-        assert DifferenceRequest.objects.count() == 0
 
 
 class PythagoreanTripletTestCase(TestCase):
@@ -56,7 +57,7 @@ class PythagoreanTripletTestCase(TestCase):
         self.url = reverse("pythagorean_triplet")
 
     def test_is_pythagorean_triplet(self):
-        assert PythagoreanTripletRequest.objects.count() == 0
+        assert cache.get('pythagorean_3_4_5') is None
 
         url = self.url + "?a=3&b=4&c=5"
         response = self.client.get(url)
@@ -66,10 +67,14 @@ class PythagoreanTripletTestCase(TestCase):
         assert set(content.keys()) == {'datetime', 'is_triplet', 'last_datetime', 'product', 'occurrences'}
         assert content["is_triplet"] is True
         assert content["product"] == 60
-        assert PythagoreanTripletRequest.objects.count() == 1
+
+        cached_value = cache.get('pythagorean_3_4_5')
+        assert cached_value['is_triplet'] is True
+        assert cached_value['product'] == 60
+        assert cached_value['occurrences'] == 1
 
     def test_is_not_pythagorean_triplet(self):
-        assert PythagoreanTripletRequest.objects.count() == 0
+        assert cache.get('pythagorean_1_2_3') is None
 
         url = self.url + "?a=1&b=2&c=3"
         response = self.client.get(url)
@@ -79,18 +84,18 @@ class PythagoreanTripletTestCase(TestCase):
         assert set(content.keys()) == {'datetime', 'is_triplet', 'last_datetime', 'product', 'occurrences'}
         assert content["is_triplet"] is False
         assert content["product"] == 6
-        assert PythagoreanTripletRequest.objects.count() == 1
+
+        cached_value = cache.get('pythagorean_1_2_3')
+        assert cached_value['is_triplet'] is False
+        assert cached_value['product'] == 6
+        assert cached_value['occurrences'] == 1
 
     def test_bad_value(self):
         url = self.url + "?a=3&b=4&c=five"
-
         response = self.client.get(url)
         assert response.status_code == 400
-        assert DifferenceRequest.objects.count() == 0
 
     def test_missing_params(self):
         url = self.url + "?a=3&b=4"
-
         response = self.client.get(url)
         assert response.status_code == 400
-        assert DifferenceRequest.objects.count() == 0

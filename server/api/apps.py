1eb630deffa7b1e375ecb2bc02f6bb7d821b393a
Zach Calvert <zacharym.calvert@gmail.com>
2023-11-02 19:33:35 -0700

    Add django-redis config

--- server/api/apps.py
+++ server/api/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.core.cache import cache
 
 
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
+
+    if not cache.get('difference'):
+        cache.set('differences', {})
+        cache.set('pythagorean_triplets', {})
--- server/backstage_challenge/settings.py
+++ server/backstage_challenge/settings.py
@@ -126,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redis config
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://cache:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
